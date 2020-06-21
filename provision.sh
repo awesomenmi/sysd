dbb81ebccc318aa582cde5369e361df771972c44
awesomenmi <walkermr@yandex.ru>
2020-06-21 17:40:07 +0300

    add jira

--- provision.sh
+++ provision.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-yum update -y
-yum install -y vim
-
 setenforce 0
+
+#monitor
 
 cp /vagrant/monitor/monitor.sh /opt/monitor.sh
 chmod +x /opt/monitor.sh
@@ -13,6 +12,8 @@ cp /vagrant/monitor/monitor.conf /etc/sysconfig/monitor
 cp /vagrant/monitor/monitor.log /var/log/monitor.log
 
 systemctl start monitor.timer
+
+#httpd
 
 yum install -y httpd
 
@@ -27,3 +28,32 @@ cp /vagrant/httpd/template.conf /etc/httpd/conf.d/template.conf
 
 systemctl start httpd@first.service
 systemctl start httpd@second.service
+
+#jira
+
+yum install wget -y
+yum install java-1.8.0-openjdk -y
+
+echo "JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.252.b09-2.el7_8.x86_64" >> /etc/enviroment
+source /etc/enviroment
+
+wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-8.5.5.tar.gz
+
+mkdir /opt/jira
+tar -xzf atlassian-jira-software-8.5.5.tar.gz -C /opt/jira --strip-components=1
+
+mkdir /var/jira
+export JIRA_HOME=/var/jira
+
+sed -i 's#jira.home =#jira.home =/var/jira#g' /opt/jira/atlassian-jira/WEB-INF/classes/jira-application.properties
+
+touch /lib/systemd/system/jira.service
+chmod 664 /lib/systemd/system/jira.service
+
+cp /vagrant/jira/jira.service /lib/systemd/system/jira.service
+chmod 664 /lib/systemd/system/jira.service
+
+systemctl enable jira
+systemctl start jira 
+
+
